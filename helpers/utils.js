f36591f726fe3524ecdd4a258e11935add51d0c8
codexmish <majharulislam.shanto.70@gmail.com>
2026-04-23 09:17:47 +0600

    generate slug added

--- helpers/utils.js
+++ helpers/utils.js
@@ -21,9 +21,19 @@ const generateAccessToken = (user) => {
   return token;
 };
 
+const generateSlug = (title) => {
+  return title
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, "") // Remove special characters
+    .replace(/[\s_-]+/g, "-") // Replace spaces and underscores with hyphens
+    .replace(/^-+|-+$/g, ""); // Remove leading/trailing hyphens;
+};
+
 module.exports = {
   isValidateEmail,
   isValidatePassword,
   generateOTP,
   generateAccessToken,
+  generateSlug,
 };
